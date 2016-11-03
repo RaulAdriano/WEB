44d688ac113cd39976d3db7edf59857b523b96cc
RaulAdriano <raul.adriano.ramos@gmail.com>
2016-11-02 23:03:41 -0200

    projeto Web

---
+++ Trabalho Web - Raul Adriano Ramos/E-commerce/src/java/br/com/modelo/Pedido.java
@@ -0,0 +1,29 @@
+package br.com.modelo;
+
+/**
+ *
+ * @author aigor - 22/10/2016
+ */
+public class Pedido {
+    
+    private int id;
+    private int idUsuario;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+}
